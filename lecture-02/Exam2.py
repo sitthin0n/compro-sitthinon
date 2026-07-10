bd87b61560ed5c65fbec5f92bcfb349866653043
sitthin0n <s6906022610801@email.kmutnb.ac.th>
2026-07-10 12:34:54 +0700

    Tung Tung

--- lecture-02/Exam2.py
+++ lecture-02/Exam2.py
@@ -2,4 +2,4 @@ weight = float(input('Enter your weight in kilograms: '))
 height = float(input('Enter your height in meters: '))
 bmi = weight/(height**2)
 
-print('bmi', format(bmi, '.2f'))
+print('Your BMI is:', format(bmi, '.2f'))
